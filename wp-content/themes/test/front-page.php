852188e8ef523db8a401b65fd720ce31aaa0a0a1
Alexandr <moyhoroshenkiy@gmail.com>
2022-07-12 19:10:39 +0800

    Open sans added, first section added

--- wp-content/themes/test/front-page.php
+++ wp-content/themes/test/front-page.php
@@ -1,5 +1,30 @@
 <?php 
 get_header('front-page');
+?>
 
+<main>
+    <section id="banner">
+        <div class="container">
+            <h1 class="heading">
+                Экономия нервов и денег <br> — интернет К-Телеком
+            </h1>
+            <ul>
+                <li>5 тарифов для многоквартирных и 4 для частных домов</li>
+                <li>безлимит подключенных устройств</li>
+                <li>комфортная поддержка 24/7</li>
+            </ul>
+            <button onclick="print(e)">Подробнее</button>
+        </div>
+    </section>
+</main>
+
+<script>
+    function print(e) {
+        e.preventDefault()
+        console.log('hi')
+    }
+</script>
+
+<?php
 get_footer('front-page');
 ?>

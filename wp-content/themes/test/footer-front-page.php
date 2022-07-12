e2c8bb12933fcc529febe3fd6a710382f3d7a58f
Alexandr <moyhoroshenkiy@gmail.com>
2022-07-12 18:48:00 +0800

    finished with footer

--- wp-content/themes/test/footer-front-page.php
+++ wp-content/themes/test/footer-front-page.php
@@ -7,7 +7,21 @@
 
         <footer>
             <div class="container">
-                <p>Footer</p>
+                <div class="row">
+                    <div class="logotype">
+                        <?php echo(get_custom_logo()); ?>
+                    </div>
+                    <div class="footer-text">
+                        <p>© 2018–2022 ООО «К-Телеком»</p>
+                    </div>
+                    <div class="socials">
+                        <ul>
+                            <li><a href="javascript:void(0)"><img src="<?php echo(get_template_directory_uri()); ?>/assets/icons/inst.png" alt=""></a></li>
+                            <li><a href="javascript:void(0)"><img src="<?php echo(get_template_directory_uri()); ?>/assets/icons/youtube.png" alt=""></a></li>
+                            <li><a href="javascript:void(0)"><img src="<?php echo(get_template_directory_uri()); ?>/assets/icons/vk.png" alt=""></a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </footer>
         <?php wp_footer(); ?>
